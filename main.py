cc24139c4a7a5ba21a77233f13c616e237dd71bd
joe33143 <42575175+joe33143@users.noreply.github.com>
2026-09-12 08:57:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ def run_sky_engine():
                 "bri": c_bri,
                 "col": [c_sky + [0], c_cloud + [0], c_sun + [0]], 
                 "cct": 127,
-                "fx": 142, "sx": target_x, "ix": c_ix, "pal": c_pal, "c1": active_alpha
+                "sx": target_x, "ix": c_ix, "pal": c_pal, "c1": active_alpha
             },
             {
                 "id": 1, 
@@ -310,13 +310,13 @@ def run_sky_engine():
                 "bri": 255,
                 "col": [[ab_val, ab_val, ab_val, 0], [0,0,0,0], [0,0,0,0]], 
                 "cct": 127,  
-                "fx": 169, "sx": 128, "ix": 128, "pal": 0, "rev": True
+                "fx": 169, "sx": 128, "ix": 128, "pal": 0, "rev": False
             },
             {
                 "id": 3, 
                 "on": bamboo_on,
                 "bri": bamboo_bri,
-                "col": [[126, 126, 126, 126], [0,0,0,0], [0,0,0,0]], 
+                "col": [[200, 200, 200, 200], [0,0,0,0], [0,0,0,0]], 
                 "cct": 127,  
                 "fx": 0, "sx": 128, "ix": 128, "pal": 0, "lc": 2
             },
